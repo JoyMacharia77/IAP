fd54549509b4f6fa6799203a856aa555aabe9fdb
Joy Macharia - 111155 - ICS B <56343054+JoyMacharia77@users.noreply.github.com>
2020-05-03 17:50:46 +0300

    Update User.php

--- lab/user.php
+++ lab/user.php
@@ -1,5 +1,6 @@
 <?php
 include "crud.php";
+include_once "DBConnector.php";
 
 class User implements Crud{
     private $user_id;
@@ -7,53 +8,74 @@ class User implements Crud{
     private $last_name;
     private $city_name;
 
-    function _construct( $first_name, $last_name, $city_name){
+    function _construct( $first_name, $last_name, $city_name)
+    {
        $this-> first_name =  $first_name;
        $this-> last_name =  $last_name;
        $this-> city_name =  $city_name;
     }
 
     //userID setter
-    public function setUserId($user_id){
+    public function setUserId($user_id)
+    {
         $this->user_id=$user_id;
     }
 
-    public function getUserId(){
-        // return $this->$user_id;
+    public function getUserId()
+    {
            return $this->user_id;
     }
 
-    public function save(){
+    public function save()
+    {
+        $con = new DBConnector();
 
-        $this->conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS) or die("Error:" .mysqli_error($this->conn));
-        mysqli_select_db ($this->conn, "btc3205");
         $fn = $this->first_name;
         $ln = $this->last_name;
         $city = $this->city_name;
-        //$res = mysqli_query($this->conn,"INSERT INTO user_table(first_name,last_name,user_city) VALUES('$fn,'$ln','$city')")or die("Error" .mysqli_error($this->conn));
-        $res = ("INSERT INTO user_table(first_name,last_name,user_city) VALUES('$fn,'$ln','$city')")or die("Error" .mysqli_error($this->conn));
-        // $query= mysqli_query($this->conn,$res);
-        mysqli_query($this->conn,$res);
-        return $res;
-       // return $query;
+        $uname = $this -> username;
+        $this->hashPassword();
+        $pass = $this ->password;
+        $res = mysqli_query($con->conn, ("INSERT INTO user_table(first_name, last_name, user_city,username,password)
+         VALUES('$fn','$ln','$city','$uname','$pass')") or die("Error : " .mysqli_error($con->conn)));
+		$con->closeDatabase();
+		return $res;
     }
 
-    public function readAll(){
+   public function readAll()
+    {
+        $con = new DBConnector();
+        $res = mysqli_query($con->conn, "SELECT * FROM user_table");
+
+        if (mysqli_num_rows($res) > 0) {
+            while ($row = mysqli_fetch_assoc($res)) {
+                echo " " .$row['id']. " " .$row['first_name']. " " .$row['last_name']. " " .$row['user_city'];
+            }
+        }
+        else {
+            echo "The Database is Empty";
+        }
+        $con->closeDatabase();
+    }
+    
+    public function readUnique()
+    {
         return null;
     }
-    public function readUnique(){
+    public function search()
+    {
         return null;
     }
-    public function search(){
+    public function update()
+    {
         return null;
     }
-    public function update(){
+    public function removeOne()
+    {
         return null;
     }
-    public function removeOne(){
-        return null;
-    }
-    public function removeAll(){
+    public function removeAll()
+    {
         return null;
     }
 }
